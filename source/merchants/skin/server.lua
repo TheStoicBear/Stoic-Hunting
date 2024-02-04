64ab2be0a6bc19b890835410a2f34aaabeea8f6f
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-04 12:59:21 -0800

    Update server.lua

--- source/merchants/skin/server.lua
+++ source/merchants/skin/server.lua
@@ -1,3 +1,13 @@
+Citizen.CreateThread(function()
+
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(100)
+    end
+	
+end)
+
+
 RegisterServerEvent('sellSkin')
 AddEventHandler('sellSkin', function(npcModel)
     local _source = source
