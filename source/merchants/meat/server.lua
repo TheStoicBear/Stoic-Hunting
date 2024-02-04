9758b0c5b649434283e36eca0f0638628c28e28e
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-04 12:59:33 -0800

    Update server.lua

--- source/merchants/meat/server.lua
+++ source/merchants/meat/server.lua
@@ -1,3 +1,12 @@
+Citizen.CreateThread(function()
+
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(100)
+    end
+	
+end)
+
 RegisterServerEvent('sellMeat')
 AddEventHandler('sellMeat', function(npcModel)
     local _source = source
